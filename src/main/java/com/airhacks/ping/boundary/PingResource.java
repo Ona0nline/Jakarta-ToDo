2f5972fa3b507ff4cf299b57cf288ac340f0f0bf
Ona0nline <onasihle123@gmail.com>
2025-12-26 20:38:46 +0200

    First

---
+++ src/main/java/com/airhacks/ping/boundary/PingResource.java
@@ -0,0 +1,26 @@
+package com.airhacks.ping.boundary;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@Path("ping")
+public class PingResource {
+
+    @Inject
+    @ConfigProperty(name = "message")
+    String message;    
+
+    @GET
+    public String ping() {
+        return this.message + " Jakarta EE with MicroProfile 2+!";
+    }
+
+}
